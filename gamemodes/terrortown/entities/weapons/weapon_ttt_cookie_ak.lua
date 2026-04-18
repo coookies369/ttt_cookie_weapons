d8150df89b691d90c3a1a5d809e36b1acc987fb3
coookies369 <37726148+coookies369@users.noreply.github.com>
2026-04-17 19:14:46 -0700

    AK focus shot

--- gamemodes/terrortown/entities/weapons/weapon_ttt_cookie_ak.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_cookie_ak.lua
@@ -28,6 +28,16 @@ SWEP.EffectiveRangeStart = 1000
 SWEP.EffectiveRangeStop = 3000
 SWEP.IneffectiveRangeMultiplier = 0.25
 
+SWEP.Focused = {
+    Cone = 0.005,
+    HeadshotMultiplier = 3,
+}
+
+SWEP.Unfocused = {
+    Cone = 0.05,
+    HeadshotMultiplier = 2,
+}
+
 -- (HP/DMG) * (60/RPM) = TTK
 -- 1
 -- 0.5 Headshot
@@ -37,11 +47,11 @@ SWEP.Primary.Recoil = 1
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "357"
 SWEP.Primary.Damage = 10
-SWEP.HeadshotMultiplier = 2
+SWEP.HeadshotMultiplier = SWEP.Unfocused.HeadshotMultiplier
 SWEP.Primary.ClipSize = 30
 SWEP.Primary.DefaultClip = 30
 SWEP.Primary.ClipMax = 60
-SWEP.Primary.Cone = 0.05
+SWEP.Primary.Cone = SWEP.Unfocused.Cone
 SWEP.Primary.Sound = Sound("weapons/ak47/ak47-1.wav")
 SWEP.SpeedModifier = 0.9
 
@@ -54,3 +64,56 @@ SWEP.WorldModel = Model("models/weapons/w_rif_ak47.mdl")
 
 SWEP.IronSightsPos = Vector(-6.625, -10, 2.7)
 SWEP.IronSightsAng = Vector(2, 0, 0)
+
+SWEP.FocusMinTime = 0
+SWEP.FocusMaxTime = 3
+
+function SWEP:SetupDataTables()
+    self:NetworkVar("Float", 0, "FocusTime")
+    BaseClass.SetupDataTables(self)
+end
+
+function SWEP:Initialize()
+    self:SetFocusTime(0)
+    BaseClass.Initialize(self)
+end
+
+function SWEP:Think()
+    local owner = self:GetOwner()
+    if not IsValid(owner) then return end
+    if SERVER then
+        if self:GetIronsights()
+            and self:GetNextPrimaryFire() < CurTime()
+            and owner:GetVelocity():IsEqualTol(Vector(0, 0, 0), 1) then
+            if self:GetFocusTime() == 0 then
+                self:SetFocusTime(CurTime())
+            end
+        else
+            -- This is a sentinal value, which you generally want to avoid
+            -- I easily COULD avoid this, but I'm not
+            self:SetFocusTime(0)
+        end
+    end
+
+    self.Primary.Cone = Lerp(self:GetFocusFraction(), self.Unfocused.Cone, self.Focused.Cone)
+    self.HeadshotMultiplier = self.Unfocused.HeadshotMultiplier
+
+    BaseClass.Think(self)
+end
+
+function SWEP:GetFocusFraction()
+    local fraction = 0
+    if self:GetFocusTime() ~= 0 then
+        fraction = (CurTime() - self:GetFocusTime())
+        fraction = math.Clamp(fraction, self.FocusMinTime, self.FocusMaxTime - self.FocusMinTime)
+        fraction = fraction - self.FocusMinTime / (self.FocusMaxTime - self.FocusMinTime)
+        fraction = fraction - 1
+    end
+    return fraction
+end
+
+if CLIENT then
+    function SWEP:CalcView(ply, pos, ang, fov)
+	    return pos, ang, fov * Lerp(self:GetFocusFraction(), 1, 0.9)
+	end
+end
