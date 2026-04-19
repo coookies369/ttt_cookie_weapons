06cb0b6d6e2ddb077e481c540e4628bdd1fa76cd
coookies369 <37726148+coookies369@users.noreply.github.com>
2026-04-18 17:26:40 -0700

    Rewrite bipod

--- gamemodes/terrortown/entities/weapons/weapon_ttt_cookie_mg.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_cookie_mg.lua
@@ -60,53 +60,51 @@ SWEP.IronSightsAng = Vector(0, 0, 0)
 SWEP.DeployPositionLimit = 4
 SWEP.DeployAngleLimit = 45
 
-SWEP.IsDeployed = false
--- Where the player was when the bipod was deployed
-SWEP.DeployPosition = nil
--- The angle the player was aiming when the bipod was deployed
-SWEP.DeployAngle = nil
--- The object we are deployed on
-SWEP.DeployMount = nil
+function SWEP:SetupDataTables()
+    self:NetworkVar("Bool", 0, "IsDeployed")
+    self:NetworkVar("Vector", 0, "DeployEyePos")
+    self:NetworkVar("Angle", 0, "DeployEyeAngles")
+    self:NetworkVar("Float", 0, "DeployFraction")
+
+    BaseClass.SetupDataTables(self)
+end
 
 function SWEP:SecondaryAttack()
-    local ent = self:GetDeployableEntity()
-    if ent ~= nil then
-        local owner = self:GetOwner()
-        if not owner:IsValid() then return end
-        self.DeployPosition = owner:GetPos()
-        self.DeployAngle = owner:EyeAngles()
-        self.DeployMount = ent
-        self.IsDeployed = true
+    local owner = self:GetOwner()
+    if not owner:IsValid() then return end
+    local frac = self:CalcDeployFraction(owner:EyePos(), owner:EyeAngles())
+    if frac ~= nil then
+        self:SetIsDeployed(true)
+        self:SetDeployEyePos(owner:EyePos())
+        self:SetDeployEyeAngles(owner:EyeAngles())
+        self:SetDeployFraction(frac)
 
         self.Primary.Recoil = self.DeployedStats.Recoil
         self.Primary.Cone = self.DeployedStats.Cone
-        self:EmitSound("weapons/tmp/tmp_clipin.wav")
+        self:EmitSound("weapons/tmp/tmp_clipin.wav", SNDLVL_30dB, 100, 1, CHAN_AUTO)
     end
     return BaseClass.SecondaryAttack(self)
 end
 
--- Return the entity we can deploy on, if any
-function SWEP:GetDeployableEntity()
+-- Perform a trace to see if we can deploy, and return the "fraction"
+-- (How far the trace went on a scale of 0 to 1)
+function SWEP:CalcDeployFraction(pos, ang)
     local owner = self:GetOwner()
     if not owner:IsValid() then return nil end
 
-    local tr = util.QuickTrace(owner:EyePos() + owner:EyeAngles():Forward() * 24, owner:EyeAngles():Up() * -16, owner)
+    local tr = util.QuickTrace(pos + ang:Forward() * 24, ang:Up() * -16, owner)
     if tr.StartSolid then return nil end
     if not tr.Hit then return nil end
-    if tr.Fraction < 0.5 then return nil end
     if tr.Entity == nil then return nil end
-    return tr.Entity
+    return tr.Fraction
 end
 
+SWEP.TraceTimer = 0
 function SWEP:Think()
-    if not self.IsDeployed then return end
+    if not self:GetIsDeployed() then return end
     local owner = self:GetOwner()
     if SERVER then
         if not owner:IsValid() then
-            self:Undeploy()
-            return
-        end
-        if not owner:GetPos():IsEqualTol(self.DeployPosition, self.DeployPositionLimit) then
             self:Undeploy()
             return
         end
@@ -114,25 +112,30 @@ function SWEP:Think()
             self:Undeploy()
             return
         end
-        if IsValid(self.DeployMount)
-            and IsValid(self.DeployMount:GetPhysicsObject())
-            and not self.DeployMount:GetPhysicsObject():IsAsleep()
-        then
+        if not owner:EyePos():IsEqualTol(self:GetDeployEyePos(), self.DeployPositionLimit) then
             self:Undeploy()
             return
         end
-        -- if owner:EyeAngles():IsEqualTol(self.DeployAngle, self.DeployAngleLimit + 5) then
-        --     self:Undeploy()
-        --     return
-        -- end
+        if not owner:EyeAngles():IsEqualTol(self:GetDeployEyeAngles(), self.DeployAngleLimit + 10) then
+            self:Undeploy()
+            return
+        end
+        -- Performing a trace every frame seems like a bad idea
+        self.TraceTimer = (self.TraceTimer + 1) % 5
+        local frac = self:CalcDeployFraction(self:GetDeployEyePos(), self:GetDeployEyeAngles())
+        if frac == nil or math.abs(frac - self:GetDeployFraction()) > 0.05 then
+            self:Undeploy()
+            return
+        end
     elseif owner == LocalPlayer() then
         -- Calculate angle between where we started aiming and where we are currently aiming
-        local angle = owner:EyeAngles():Forward():Dot(self.DeployAngle:Forward())
-        angle = angle / (owner:EyeAngles():Forward():Length() * self.DeployAngle:Forward():Length())
+        local deploy_angle = self:GetDeployEyeAngles()
+        local angle = owner:EyeAngles():Forward():Dot(deploy_angle:Forward()) / 1
+        -- angle = angle / (owner:EyeAngles():Forward():Length() * deploy_angle:Forward():Length())
         angle = math.deg(math.acos(angle))
         -- If we have turned too far, correct it
         if angle > self.DeployAngleLimit then
-            local new_angles = LerpAngle(self.DeployAngleLimit / angle, self.DeployAngle, owner:EyeAngles())
+            local new_angles = LerpAngle(self.DeployAngleLimit / angle, deploy_angle, owner:EyeAngles())
             new_angles.roll = 0
             owner:SetEyeAngles(new_angles)
         end
@@ -140,14 +143,13 @@ function SWEP:Think()
 end
 
 function SWEP:Undeploy()
-    if not self.IsDeployed then return end
-    self:EmitSound("weapons/tmp/tmp_clipout.wav")
-    self.IsDeployed = false
+    self:EmitSound("weapons/tmp/tmp_clipout.wav", SNDLVL_30dB, 100, 1, CHAN_AUTO)
+    self:SetIsDeployed(false)
     self.Primary.Recoil = self.UndeployedStats.Recoil
     self.Primary.Cone = self.UndeployedStats.Cone
     if SERVER then
         net.Start("UndeployMG")
-        net.WriteEntity(Entity(self:EntIndex()))
+        net.WriteEntity(self)
         net.Broadcast()
     end
 end
@@ -172,18 +174,22 @@ end
 
 if CLIENT then
     local bipod = Material("gui/cookie/mg_bipod.png", "noclamp smooth")
+    local bipod_deployed = Material("gui/cookie/mg_bipod_deployed.png", "noclamp smooth")
 
     function SWEP:DrawHUD()
-        if self:GetDeployableEntity() ~= nil then
-            surface.SetDrawColor(LocalPlayer().GetRoleColor and LocalPlayer():GetRoleColor() or roles.INNOCENT.color)
-
-            local scrW = ScrW()
-            local scrH = ScrH()
-            local x = 0.5 * scrW
-            local y = 0.5 * scrH
-
-            surface.SetMaterial(bipod)
+        surface.SetDrawColor(LocalPlayer().GetRoleColor and LocalPlayer():GetRoleColor() or roles.INNOCENT.color)
+        local scrW = ScrW()
+        local scrH = ScrH()
+        local x = 0.5 * scrW
+        local y = 0.5 * scrH
+        if self:GetIsDeployed() then
+            surface.SetMaterial(bipod_deployed)
             surface.DrawTexturedRect(x - scrH / 32, y + scrH / 16, scrH / 16, scrH / 16, 0)
+
+        -- TODO: none of this works and I don't know why :3
+        -- elseif self:CalcDeployFraction(LocalPlayer():GetPos(), LocalPlayer():EyeAngles()) ~= nil then
+        --     surface.SetMaterial(bipod)
+        --     surface.DrawTexturedRect(x - scrH / 32, y + scrH / 16, scrH / 16, scrH / 16, 0)
         end
         return BaseClass.DrawHUD(self)
     end
